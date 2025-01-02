71e7d62883133fe0a037bc90fd62a52fa9432e02
ClayplugLLC <114144588+ClayplugLLC@users.noreply.github.com>
2025-01-02 05:00:49 -0500

    test package

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "com.clayplug"
+version = "0.0.3"
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ClayplugLLC/OSBTree")
+            credentials {
+                username = System.getenv("USERNAME")
+                password = System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
